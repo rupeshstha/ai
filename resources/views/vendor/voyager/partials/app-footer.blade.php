9f79efda44952344621b21a22441f14225fde008
Bikram <thebikramlama@gmail.com>
2019-05-31 00:41:27 +0545

    Footer Fix

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        {!! __('voyager::theme.footer_copyright') !!} <a href="https://bikramlama.com.np" target="_blank">Bikram Lama</a>
+    </div>
+</footer>
